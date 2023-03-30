eb68fe9aa11b474f8c16c6c2126d08ca4203d60d
Karl Garratt <garratt.karl@elev.ga.ntig.se>
2023-03-30 11:11:20 +0200

    Smol update no.1

--- src/Box.java
+++ src/Box.java
@@ -5,19 +5,21 @@ public class Box {
     // Attributes
     final int WIDTH = 500;
     final int HEIGHT = 520;
+    final int ROWS = 30;
+    final int COLS = 30;
     // Constructors
     public Box(){
-// Normal JFrame stuff
+        // Normal JFrame stuff
         JFrame frame = new JFrame();
         frame.setTitle("Snake");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(new Dimension(WIDTH, HEIGHT));
 
         // 2 Panels in grid
-        JPanel dual = new JPanel();
-        dual.setBorder(BorderFactory.createLineBorder((Color.BLACK), 10));
-        LayoutManager layout = new GridLayout(2,1);
-        dual.setLayout(layout);
+        JPanel mix = new JPanel();
+        mix.setBorder(BorderFactory.createLineBorder((Color.BLACK), 10));
+        LayoutManager layout = new GridLayout(ROWS,COLS);
+        mix.setLayout(layout);
 
         // Finishing (important) touches
         frame.add(dual);
