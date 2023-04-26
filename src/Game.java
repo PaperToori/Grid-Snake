c851164cb0b7ae570418b8eeca307af8e9a17a74
Karl Garratt <garratt.karl@elev.ga.ntig.se>
2023-04-26 13:26:38 +0200

    body is back, timer needs to be added/updateSnek() made relevant.

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.awt.event.*;
 
@@ -7,15 +8,21 @@ import java.awt.event.*;
 public class Game extends JPanel implements KeyListener {
 
     // Attributes
-
     Panel[] panels;
     int snekHead;
     List<Integer> snek;
+    String direction; // use this to make movement dependent on updateSnek() instead of Keylistener.
+    int length;
+
     // Constructors
     public Game(int SIDE){
 
-        // Places snek in the top left corner
+        // Sets initial snake settings
         snekHead = 1;
+        length = 1;
+        snek = new ArrayList<Integer>();
+
+
 
         // The panels necessary to fill the grid
         panels = new Panel[(SIDE*SIDE)];
@@ -23,16 +30,54 @@ public class Game extends JPanel implements KeyListener {
             panels[i] = new Panel((i+1));
 
             // The border helps test if the grid is working as it should
-            panels[i].setBorder(BorderFactory.createLineBorder((Color.green),3));
+            panels[i].setBorder(BorderFactory.createLineBorder((Color.green),1));
 
             this.add(panels[i]);
-
         }
 
     }
 
     // Methods
 
+    private void spotHead(){
+        for (Panel p: panels)
+        {
+            if (p.id == snekHead){p.setState(1);}
+            else {p.setState(0);}
+            p.stain();
+        }
+    }
+    private void spotSnek(){
+
+        if (!snek.contains(snekHead))
+        {snek.add(snekHead);}
+
+        if (snek.size() > length) {snek.remove(0);}
+        for(Integer i : snek){
+            System.out.println(i);
+        }
+        System.out.println("Length = " + length);
+
+        for(Panel p : panels){
+            if (snek.contains(p.id) || p.id== snekHead){p.setState(1);}
+            else {p.setState(0);}
+            p.stain();
+        }
+
+    }
+    private void updateSnek(){
+        switch (direction) {
+            case "Left"
+                    -> snekHead -=1;  // Left
+            case "Right"
+                    -> snekHead += 1;  // Right
+            case "Up"
+                    -> snekHead -=25; // Up
+            case "Down"
+                    -> snekHead +=25; // Down
+        }
+
+    }
 
 
     // G/Set
@@ -42,33 +87,20 @@ public class Game extends JPanel implements KeyListener {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
 
-        for (Panel p: panels)
-        {
-            if (p.id == snekHead){p.setState(1);}
-            else {p.setState(0);}
-            p.stain();
-        }
+        spotHead();
+        spotSnek();
 
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-
         switch (e.getKeyChar()) {
-            case 'a' ->  // Left
-                    snekHead -=1;
-            // System.out.println("Left");
-            case 'd' -> // Right
-                    snekHead +=1;
-            // System.out.println("Right");
-            case 'w' ->    // Up
-                    snekHead -=25;
-            //System.out.println("Up");
-            case 's' -> // Down
-                    snekHead +=25;
-            //System.out.println("Down");
+            case 'a' -> snekHead -=1;  // Left
+            case 'd' -> snekHead +=1;  // Right
+            case 'w' -> snekHead -=25; // Up
+            case 's' -> snekHead +=25; // Down
+            case 'f' -> length += 1;   // Free food
         }
-
         repaint();
     }
     @Override
